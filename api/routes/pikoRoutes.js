d86f1e198edac7608e1a0957adb6576b01db0c99
Panuci <vitorpanuci@gmail.com>
2018-05-30 15:10:51 -0300

    Removed unused route

--- api/routes/pikoRoutes.js
+++ api/routes/pikoRoutes.js
@@ -2,11 +2,6 @@
 module.exports = function(app) {
   var pikoController = require('../controllers/pikoController');
 
-	app.get('/facefile', function(req, res){
-		var file = __dirname + '/../../facefinder';
-		res.download(file);
-	});
-
   // findface Route
   app.route('/findface').post(pikoController.find_face);
 };
